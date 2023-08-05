087f5720b209377e00024f0b97eb3ff0d341c487
Olasoji <olasojiige@gmail.com>
2023-08-05 09:16:09 +0100

    Completed request body parsing and validation.

--- lets go further/greenlight/internal/data/movies.go
+++ lets go further/greenlight/internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"greenlight.olasoji.net/internal/validator"
 	"time"
 )
 
@@ -23,4 +24,18 @@ type Movie struct {
 	// Movie runtime (in minutes)
 	// Slice of genres for the movie (romance, comedy, etc.)
 	// The version number starts at 1 and will be incremented each // time the movie information is updated
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
